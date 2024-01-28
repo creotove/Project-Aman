4914a1a9e37e87963b4aa347e68b0869edc7c6d2
1altamas <67779413+1altamas@users.noreply.github.com>
2024-01-29 03:39:34 +0530

    Update MongoDB connection URL

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}/Aman`);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
